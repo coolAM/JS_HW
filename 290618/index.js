304862bcb0cc6895e33a1637040ceb48cd0936d6
coolAM <mischanton@gmail.com>
2018-07-03 21:51:11 +0300

    290618HW continue

---
+++ 290618/index.js
@@ -0,0 +1,13 @@
+/*document.addEventListener('DOMContentLoaded', function(){
+        const GalleryPlugin = function(rootelem, options = {}) {
+            this.previewImgSelector = options.previewImgSelector ? options.previewImgSelector : '[data-my_gallery="preview_img"]'
+            this.mainImgSelector = options.mainImgSelector ? options.mainImgSelector : '[data-my_gallery="main_img"]'
+            let previewImgElems = rootelem.querySelectorAll(this.previewImgSelector);
+        }
+        let galleries = document.querySelector('[data-toogle="mygallery"]');
+        for(let item of galleries) {
+            let gallery = new GalleryPlugin(item);
+        }
+})
+let elementList = document.querySelectorAll("list_element");
+console.log(elementList);*/
