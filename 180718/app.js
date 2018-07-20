329a0a0f0fe72da5226fe9c4917eddbbebf7c0ba
coolAM <mischanton@gmail.com>
2018-07-20 17:12:00 +0300

    C:/Program Files/Git/index.html - download file

--- 180718/app.js
+++ 180718/app.js
@@ -1,9 +1,14 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     if(req.url === '/') {
         res.write('Hello World');
         res.end();
+    }
+    if(req.url=="/index.html"){
+        res.writeHead(200, {"Content-Type" : "application/html"})
+        fs.createReadStream("index.html").pipe(res);
     }
 });
 
